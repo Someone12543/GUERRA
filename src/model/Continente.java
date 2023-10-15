717f7b3fd7a418b17411b6171f00e9eeb426f9f7
Lyinno <109544399+Lyinno@users.noreply.github.com>
2023-10-15 11:41:32 -0300

    adicionando atributo bonus a continente

--- src/model/Continente.java
+++ src/model/Continente.java
@@ -5,11 +5,13 @@ class Continente {
 	String nome;
 	Continentes tipo;
 	ArrayList<Territorio> paises;
+	int bonus;
 	
-	public Continente(String nome, Continentes tipo, ArrayList<Territorio> paises) {
+	public Continente(String nome, Continentes tipo, ArrayList<Territorio> paises, int bonus) {
 		this.nome = nome;
 		this.tipo = tipo;
 		this.paises = paises;
+		this.bonus = bonus;
 	}
 	
 	public enum Continentes
